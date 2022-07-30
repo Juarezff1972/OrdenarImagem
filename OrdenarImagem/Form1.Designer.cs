3143ecc04c2c973e85bf1737e9eae25ad2b26145
Juarezff1972 <110309684+Juarezff1972@users.noreply.github.com>
2022-07-30 18:45:38 -0300

    Add files via upload

---
+++ OrdenarImagem/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace OrdenarImagem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && ( components != null ))
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(610, 478);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1132, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Abrir Imagem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(1132, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ordenar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 489);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1214, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(1132, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Filtro";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1238, 524);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ OrdenarImagem/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrdenarImagem
+{
+    public partial class Form1 : Form
+    {
+        private Image img;
+        private Bitmap bmp;
+        private Graphics graph;
+        private float ratio;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ratio = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //bmp = new Bitmap(openFileDialog1.FileName);
+
+                Bitmap orig = new Bitmap(openFileDialog1.FileName);
+                bmp = new Bitmap(orig.Width, orig.Height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+
+                using (Graphics gr = Graphics.FromImage(bmp))
+                {
+                    gr.DrawImage(orig, new Rectangle(0, 0, bmp.Width, bmp.Height));
+                }
+
+                ratio = (float)bmp.Width / (float)bmp.Height;
+
+                pictureBox1.Image = bmp;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Width = (int)(pictureBox1.Height * ratio);
+                button2.Enabled = true;
+                orig.Dispose();
+            }
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+            pictureBox1.Width = (int)(pictureBox1.Height * ratio);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Color c;
+            List<Point>[] clr;
+            Point p;
+            int x, y;
+            int idx, j;
+
+            /*long l1, l2, l3;
+            int x1, y1, z1;
+            int x2, y2, z2;
+            long perim;*/
+            clr = new List<Point>[16777216];
+            for (x = 0; x < bmp.Width; x++)
+            {
+                for (y = 0; y < bmp.Height; y++)
+                {
+                    c = bmp.GetPixel(x, y);
+                    p = new Point(x, y);
+                    idx = c.B + c.G * 256 + c.R * 256 * 256;
+                    // idx = (int)Math.Sqrt(c.B * c.B + c.G * c.G + c.R * c.R);
+                    /*x1 = c.B; y1 = 0;   z1 = 0;
+                    x2 = 0;   y2 = c.G; z2 = 0;
+                    l1 = (long)Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (x2 - z1));
+                    x1 = 0; y1 = c.G; z1 = 0;
+                    x2 = 0; y2 = 0;   z2 = c.R;
+                    l2 = (long)Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (x2 - z1));
+                    x1 = 0;   y1 = 0; z1 = c.R;
+                    x2 = c.B; y2 = 0; z2 = 0;
+                    l3 = (long)Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (x2 - z1));
+
+                    perim = (l1 + l2 + l3);
+                    idx = (int)Math.Sqrt(perim * (perim - l1) * (perim - l2) * (perim - l3));*/
+
+
+                    //idx = ( c.R + c.G + c.B ) / 3;
+                    if (clr[idx] == null)
+                    {
+                        clr[idx] = new List<Point>();
+                    }
+                    clr[idx].Add(p);
+                    c = Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B);
+                    bmp.SetPixel( x, y, c );
+                }
+                Application.DoEvents();
+                pictureBox1.Refresh();
+            }
+            //pictureBox1.Image = bmp;
+            x = 0;
+            y = 0;
+            progressBar1.Maximum = 16777216;
+            progressBar1.Value = 0;
+            for (idx = 0; idx < 16777216; idx++)
+            {
+                if (clr[idx] != null)
+                {
+                    c = Color.FromArgb(idx);
+                    for (j = 0; j < clr[idx].Count; j++)
+                    {
+                        bmp.SetPixel(x, y, Color.FromArgb(c.R, c.G, c.B));
+                        x++;
+                        if (x >= bmp.Width)
+                        {
+                            x = 0;
+                            y++;
+                        }
+                    }
+                }
+                Application.DoEvents();
+
+                if (idx % 2048 == 0)
+                {
+                    pictureBox1.Image = bmp;
+                    pictureBox1.Refresh();
+                }
+
+                //pictureBox1.Refresh();
+                progressBar1.Value = idx;
+            }
+            pictureBox1.Image = bmp;
+            pictureBox1.Refresh();
+        }
+
+        private Color aplicaFiltro(int x, int y, sbyte[,] filtro1, sbyte[,] filtro2, Bitmap bmp1)
+        {
+            int i, j;
+            int k, l;
+            int r1, g1, b1;
+            int r2, g2, b2;
+            double r, g, b;
+
+            sbyte f1;
+            sbyte f2;
+
+            Color c1;
+            r1 = 0;
+            g1 = 0;
+            b1 = 0;
+            r2 = 0;
+            g2 = 0;
+            b2 = 0;
+
+            //t = 0;
+            for (i = -1; i < 2; i++)
+            {
+                for (j = -1; j < 2; j++)
+                {
+                    k = x + i;
+                    l = y + j;
+                    if ((k < 0) || (l < 0) || (k >= bmp.Width) || (l >= bmp.Height))
+                    {
+                        c1 = Color.FromArgb(0, 0, 0);
+                    }
+                    else
+                    {
+                        c1 = bmp1.GetPixel(k, l);
+                    }
+                    f1 = filtro1[i + 1, j + 1];
+                    f2 = filtro2[i + 1, j + 1];
+
+                    r1 += (c1.R * f1);// / 4;
+                    g1 += (c1.G * f1);// / 4;
+                    b1 += (c1.B * f1);// / 4;
+                    r2 += (c1.R * f2);// / 4;
+                    g2 += (c1.G * f2);// / 4;
+                    b2 += (c1.B * f2);// / 4;
+
+                }
+            }
+
+            r = Math.Sqrt(r1 * r1 + r2 * r2);
+            g = Math.Sqrt(g1 * g1 + g2 * g2);
+            b = Math.Sqrt(b1 * b1 + b2 * b2);
+
+
+            if (r > 255)
+                r = 255;
+            if (r < 0)
+                r = 0;
+            if (g > 255)
+                g = 255;
+            if (g < 0)
+                g = 0;
+            if (b > 255)
+                b = 255;
+            if (b < 0)
+                b = 0;
+
+            c1 = Color.FromArgb((int)r, (int)g, (int)b);
+
+            return c1;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sbyte[,] filtro1;
+            sbyte[,] filtro2;
+            int x, y;
+            Color c;
+            Bitmap bmp1;
+            filtro1 = new sbyte[3, 3];
+            filtro1[0, 0] = 1;
+            filtro1[0, 1] = 0;
+            filtro1[0, 2] = -1;
+            filtro1[1, 0] = 2;
+            filtro1[1, 1] = 0;
+            filtro1[1, 2] = -2;
+            filtro1[2, 0] = 1;
+            filtro1[2, 1] = 0;
+            filtro1[2, 2] = -1;
+            filtro2 = new sbyte[3, 3];
+            filtro2[0, 0] = -1;
+            filtro2[0, 1] = -2;
+            filtro2[0, 2] = -1;
+            filtro2[1, 0] = 0;
+            filtro2[1, 1] = 0;
+            filtro2[1, 2] = 0;
+            filtro2[2, 0] = 1;
+            filtro2[2, 1] = 2;
+            filtro2[2, 2] = 1;
+            progressBar1.Maximum = bmp.Width;
+            progressBar1.Value = 0;
+            bmp1 = new Bitmap(bmp.Width, bmp.Height);
+
+            for (x = 0; x < bmp.Width; x++)
+            {
+                for (y = 0; y < bmp.Height; y++)
+                {
+                    bmp1.SetPixel(x, y, bmp.GetPixel(x, y));
+                }
+            }
+
+            for (x = 0; x < bmp.Width; x++)
+            {
+                for (y = 0; y < bmp.Height; y++)
+                {
+                    //c = bmp.GetPixel( x, y );
+                    c = aplicaFiltro(x, y, filtro1, filtro2, bmp1);
+                    bmp.SetPixel(x, y, c);
+
+                }
+                pictureBox1.Image = bmp;
+                progressBar1.Value = x;
+                pictureBox1.Refresh();
+                //pictureBox1.Refresh();
+            }
+            pictureBox1.Refresh();
+        }
+    }
+}
